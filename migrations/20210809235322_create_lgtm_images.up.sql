fe5b4d3d2f2cda9836488f061b936c5ddf0c5179
kobayashi-m42 <32682645+kobayashi-m42@users.noreply.github.com>
2021-08-19 23:54:20 +0900

    Update migrations/20210809235322_create_lgtm_images.up.sql
    Co-authored-by: keita-koga <keita.koga.work@gmail.com>

--- migrations/20210809235322_create_lgtm_images.up.sql
+++ migrations/20210809235322_create_lgtm_images.up.sql
@@ -1,7 +1,11 @@
 CREATE TABLE `lgtm_images` (
-                               `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
-                               `path_name` varchar(60) NOT NULL,
-                               `created_at` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
-                               `updated_at` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
-                               PRIMARY KEY (`id`)
+  `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
+  `uuid` varchar(255) NOT NULL COMMENT '画像ファイル名になっているUUID形式の文字列が格納される .e.g. aaaaaaaa-aaaa-aaaa-aaaa-123456788qqq',
+  `path` varchar(255) NOT NULL COMMENT 'S3に保存されているパス .e.g. YYYY/MM/DD/H',
+  `created_at` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `updated_at` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `uq_lgtm_images_01` (`uuid`),
+  KEY `idx_lgtm_images_01` (`path`),
+  KEY `idx_lgtm_images_02` (`created_at`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_bin ROW_FORMAT=DYNAMIC;
