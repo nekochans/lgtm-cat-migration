a5b64692545ef84834350c363a371df061763158
kobayashi-m42 <32682645+kobayashi-m42@users.noreply.github.com>
2021-08-29 23:11:15 +0900

    Update migrations/20210809235322_create_lgtm_images.up.sql
    Co-authored-by: keita-koga <keita.koga.work@gmail.com>

--- migrations/20210809235322_create_lgtm_images.up.sql
+++ migrations/20210809235322_create_lgtm_images.up.sql
@@ -1,7 +1,7 @@
 CREATE TABLE `lgtm_images` (
   `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
   `filename` varchar(255) NOT NULL COMMENT '画像ファイル名になっているuniqueな文字列が格納される .e.g. aaaaaaaa-aaaa-aaaa-aaaa-123456788qqq',
-  `path` varchar(255) NOT NULL COMMENT 'S3に保存されているパス .e.g. YYYY/MM/DD/H',
+  `path` varchar(255) NOT NULL COMMENT 'S3に保存されているパス .e.g. YYYY/MM/DD/HH',
   `created_at` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
   `updated_at` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
   PRIMARY KEY (`id`),
